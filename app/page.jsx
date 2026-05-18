d0b0f0895b5f15f8a5d49ca530094f722cc0da26
megellio <the.megan.raney@gmail.com>
2026-05-18 11:32:24 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,8 @@ export default function Page() {
   return (
     <div style={{ padding: 20 }}>
       <h1>✅ Your app is LIVE</h1>
+      <p>If you see this, deployment is fixed.</p>
     </div>
   );
 }
+``
